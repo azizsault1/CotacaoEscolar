9ddafd4f7b02deffd5c5c83156a6fba44dcda8a2
Alexandre da Hora do Sacramento <ra00267335@pucsp.edu.br>
2019-10-26 11:13:58 -0200

    Remoção de item na lista de materiais de uma escola e serie

--- src/main/java/cotacaoEscolar/controller/ControllerAlteracaoRest.java
+++ src/main/java/cotacaoEscolar/controller/ControllerAlteracaoRest.java
@@ -118,10 +118,19 @@ public class ControllerAlteracaoRest {
 
    @CrossOrigin(origins = "*")
    @ApiOperation(value = "Adiciona um item em uma escola e serie.")
-   @PostMapping(value = "item/{escola}/{serie}", produces = "application/json", consumes = "application/json")
+   @DeleteMapping(value = "item/{escola}/{serie}", produces = "application/json", consumes = "application/json")
    public Item adicionarItem(@PathVariable final String escola, @PathVariable final String serie, @RequestBody final Item item) throws FoiNao {
       final Serie serieModel = new Serie(escola, serie);
       this.servicoListaMaterial.adicionar(serieModel.getEscolaModel(), serie, item);
+      return item;
+   }
+
+   @CrossOrigin(origins = "*")
+   @ApiOperation(value = "Remove um item em uma escola e serie.")
+   @DeMapping(value = "item/{escola}/{serie}", produces = "application/json", consumes = "application/json")
+   public Item removerItem(@PathVariable final String escola, @PathVariable final String serie, @RequestBody final Item item) throws FoiNao {
+      final Serie serieModel = new Serie(escola, serie);
+      this.servicoListaMaterial.remover(serieModel.getEscolaModel(), serie, item);
       return item;
    }
 
